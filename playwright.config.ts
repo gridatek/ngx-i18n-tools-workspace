73de8f883465ea757276f027760ee37e293ca4e7
kgridou <32600911+kgridou@users.noreply.github.com>
2025-10-19 18:08:11 +0200

    wip

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,9 +7,9 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  forbidOnly: !!process.env['CI'],
+  retries: process.env['CI'] ? 2 : 0,
+  workers: process.env['CI'] ? 1 : undefined,
   reporter: 'html',
 
   use: {
@@ -52,7 +52,7 @@ export default defineConfig({
   webServer: {
     command: 'npm run serve:demo',
     url: 'http://localhost:4200',
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env['CI'],
     timeout: 120000,
   },
 });
